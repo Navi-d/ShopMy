2af6275c41615b8b77835ba3b50a9dcc5ccbae92
a7m-1st <Ahmed.jimi.awelkeir500@gmail.com>
2024-05-25 20:09:14 +0800

    Bread Crumbs

---
+++ frontend/src/components/Common/BreadCrumbs.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Meta from './Meta';
+
+
+const BreadCrumbs = (props) => {
+  const {title} = props;
+  return (
+    <>
+        <Meta title={title} />
+        <div class="breadcrumb py-3 mb-0">
+            <div class="container-xxl">
+                <div class="row">
+                    <div class="col-12 align-items-center">
+                        <p className='text-center'>
+                            <Link className='a ' to="/home" class="text-dark">
+                               Home {" "}
+                            </Link>
+                            / {title} 
+                            
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+    
+  )
+}
+
+export default BreadCrumbs
